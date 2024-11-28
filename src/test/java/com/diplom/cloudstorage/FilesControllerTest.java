007cc7ed61996604f7e5df3ade4d6837329324e6
Nastya B <nastybondareva@yandex.ru>
2024-11-28 19:15:57 +0300

    added tests

--- src/test/java/com/diplom/cloudstorage/FilesControllerTest.java
+++ src/test/java/com/diplom/cloudstorage/FilesControllerTest.java
@@ -29,7 +29,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-
 public class FilesControllerTest {
     @Autowired
     private MockMvc mockMvc;
