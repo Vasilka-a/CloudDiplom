44afa5836722328b7858fed4def941fa6d4459aa
Nastya B <nastybondareva@yandex.ru>
2024-11-29 13:52:15 +0300

    initial commit

--- src/main/java/com/diplom/cloudstorage/repository/FilesRepository.java
+++ src/main/java/com/diplom/cloudstorage/repository/FilesRepository.java
@@ -4,10 +4,12 @@ import com.diplom.cloudstorage.entity.Files;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface FilesRepository extends JpaRepository<Files, Long> {
 
     @Query("SELECT f FROM Files f WHERE f.user.id = :id ORDER BY f.createdAt DESC LIMIT :limit")
